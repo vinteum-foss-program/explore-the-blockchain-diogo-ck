523937855d3e419fb21a18a0d343325f85c365d6
diogo-ck <diogock@gmail.com>
2024-11-23 20:25:40 -0300

    resolution

--- submission/007.sh
+++ submission/007.sh
@@ -1,1 +1,15 @@
 # Only one single output remains unspent from block 123,321. What address was it sent to?
+blockhash=$(bitcoin-cli getblockhash 123321)
+for tx in $(bitcoin-cli getblock $blockhash 2 | jq -rc 'walk(if type == "string" then gsub("[ \\n\\t]"; "-") else . end) | .tx[]')
+do
+  txid=$(echo $tx | jq -r .txid)
+  for n in $(echo $tx | jq -rc .vout[].n)
+  do
+    txout=$(bitcoin-cli gettxout $txid $n)
+    if [ ! -z "$txout" ]; then
+      echo $txout | jq -r .scriptPubKey.address
+      exit 0
+    fi
+  done
+done
+
--- submission/008.sh
+++ submission/008.sh
@@ -1,2 +1,4 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
+wit=$(bitcoin-cli getrawtransaction e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163 true | jq -r .vin[].txinwitness[-1])
+bitcoin-cli decodescript $wit | jq -r '.asm | split(" ") | if .[0] == "OP_IF" then .[1] end'
